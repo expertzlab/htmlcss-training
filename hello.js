c7628ae3282eb035bbfe1490791f50da6d0f1eae
Gireesh Babu <gireesh.babu@nibodha.com>
2017-09-11 16:36:03 +0530

    initial import

---
+++ hello.js
@@ -0,0 +1,7 @@
+/**
+ * Created by gireeshbabu on 14/08/17.
+ */
+
+function hello(){
+    alert("From jaavascript file");
+}
---
+++ server/test.ts.js
@@ -0,0 +1,7 @@
+/**
+ * Created by gireeshbabu on 17/08/17.
+ */
+
+(function test(){
+    console.log("hello world");
+}());
